65fa7cb57cba07826a5717e7fc6cd152d2f4db6e
Victor De Abreu <vmanuel.deabreu1@gmail.com>
2019-09-30 11:30:23 -0300

    Adicionando cards e ajuste no header

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+    $nomeSistema = " Digital House";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <?php 
+        // $nome = "Victor";
+        // $idade = 28;
+        // $usuario = ["Victor","De Abreu",28, ["musica"]];
+
+    
+        // $usuarioDois = ["nome"=>"Victor","sobrenome"=>"De Abreu"];
+
+        // var_dump($usuario);
+
+        // $usuario[] = "Guitarra";
+
+        // echo "<br>";
+
+        // var_dump($usuario);
+        
+        // echo "<hi>".$usuario[0]." ".$usuario[1]"</h1>";
+        
+
+        // echo $usuarioDois["sobrenome"];
+    ?>
+<header class="navbar">
+    <h1 id="logo">
+        <?php echo $nomeSistema; ?>
+    </h1>
+    <nav>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Curso</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Cadastrar</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <section class="container mt-4">
+        <div class="row justify-content-around">
+           <div class="col-lg-3 card text-center">
+                <h2>Titulo produto</h2>
+                <img src="img/curso.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">R$150,00</h5>
+                    <a href="#" class="btn btn-primary">Comprar curso</a>
+                </div>
+           </div>
+           <div class="col-lg-3 card text-center">
+                <h2>Titulo produto</h2>
+                <img src="img/curso.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">R$150,00</h5>
+                    <a href="#" class="btn btn-primary">Comprar curso</a>
+                </div>
+           </div>
+           <div class="col-lg-3 card text-center">
+                <h2>Titulo produto</h2>
+                <img src="img/curso.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">R$150,00</h5>
+                    <a href="#" class="btn btn-primary">Comprar curso</a>
+                </div>
+           </div>
+        </div>
+    </section>
+</main>
+
+</body>
+</html>
